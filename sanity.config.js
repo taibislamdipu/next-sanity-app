dbada6f78138d7a81a7c94c2697285b375b65bf5
taibislamdipu <mailtaibislam@gmail.com>
2023-09-30 17:07:28 +0600

    schema add.

--- sanity.config.js
+++ sanity.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
   name: "default",
@@ -13,6 +14,8 @@ const config = defineConfig({
   basePath: "/admin",
 
   plugins: [deskTool()],
+
+  schema: { types: schemas },
 });
 
 export default config;
